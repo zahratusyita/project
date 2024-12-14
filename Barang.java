00452f560ae1c65e882c1aaa3023ad8bd48f2d0b
F1D02310011_Irfan Jayadi <irfanjayadi2004@gmail.com>
2024-12-14 20:24:06 +0800

    mengisi class barang

--- Barang.java
+++ Barang.java
@@ -1,3 +1,15 @@
 public class Barang {
-    
+    String id;
+    String nama;
+    String kategori;
+    int jumlah_stok;
+    int harga_satuan;
+
+    Barang(String id, String nama, String kategori, int jumlah_stok, int harga_satuan) {
+        this.id = id;
+        this.nama = nama;
+        this.kategori = kategori;
+        this.jumlah_stok = jumlah_stok;
+        this.harga_satuan = harga_satuan;
+    }
 }
